9b816fd33e429d39fa46dd369293c21fb599f2c7
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-01-21 16:17:58 +0200

    hw6
    hw6

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
-// import { Provider } from 'react-redux';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import './App.css';
-// import {store} from './store/index';
+import {store} from './store/index';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactsList from './components/ContactList/ContactList';
+import useLocalStorage from './components/uselocalstorage/uselocalstorage';
+// import Modal from './components/Modal/Modal';
+import './App.css';
+
 
 function App() {
- 
 
-  return (
- 
-    <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>  
 
-      
-    
-    
-    
+  const [filter, setFilter] = useState ('');
+const [contacts, setContacts] = useLocalStorage('contacts', []);
+
+const formSubmitData = ({ name, number }) => {
+  const newItem = { id: nanoid(), name: name, number: number };
+  let isUnique = contacts.some(el => el.name === name);
+  if (!isUnique) {
+    setContacts(prevContacts => {
+      return [...prevContacts, newItem];
+    });
+  } else {
+    alert(`${name} is already in contacts`);
+  }
+};
+
+const changeFilter = e => {
+  setFilter(e.currentTarget.value);
+};
+
+const renderContacts = () => {
+  const toLowerCaseFilter = filter.toLowerCase();
+  return contacts.filter(el =>
+    el.name.toLowerCase().includes(toLowerCaseFilter),
   );
+};
+
+const deleteContact = id => {
+  setContacts(prevContacts => {
+    return prevContacts.filter(el => el.id !== id);
+  });
+};
+
+return (
+  <Provider store={store}>
+<main className="main">
+    <h1 className="title">Phonebook</h1>
+    <ContactForm onSubmit={formSubmitData} />
+    <h2 className="title">Contacts</h2>
+    <Filter value={filter} onChange={changeFilter} />
+    <ContactsList
+      renderContacts={renderContacts()}
+      deleteContact={deleteContact}
+    />
+  </main>
+  </Provider>
+  
+);
 }
 
 export default App;
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <div className={styles.filter}>
+      <label className={styles.label}>
+        Find contacts by name
+        <input
+          className={styles.input}
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+};
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
