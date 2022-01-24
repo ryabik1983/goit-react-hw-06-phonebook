f53a0c2366fd7d9f91491f845486f40fefde70d5
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-01-24 16:15:33 +0200

    hw6
    hw6

--- src/store/index.js
+++ src/store/index.js
@@ -2,42 +2,48 @@ import {createStore, applyMiddleware} from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
 const initialStore = {
-    cart: 'empty',
-    balance: 100,
+    contacts: {
+      items: [],
+      filter: ''
     }
-const composeEnhancers = composeWithDevTools({
+  }
+
+//   configureStore()
+// createAction()
+
+  const composeEnhancers = composeWithDevTools({
 name: 'DO_NOT_FOLLOV_DIRTY_LINKS',
       });
 
 const ACTIONS = {
-        ADD_TO_CART: 'ADD_TO_CART',
-        CLEAR_CART: 'CLEAR_CART',
-        CHECKOUT: 'CHECKOUT'
+        ADD_CONTACT: 'ADD_CONTACT',
+        DELETE_CONTACT: 'DELETE_CONTACT',
+        FILTER: 'FILTER'
     }
     export const addToCart = () => ({
         type: ACTIONS.ADD_TO_CART,
     })
 
-    const reduser = (state = initialStore, action) => {
+function createReducer (state = initialStore, action) {
             switch (action.type) {
-                case ACTIONS.ADD_TO_CART:
+                case ACTIONS.ADD_CONTACT:
                     return {...state, balance: state.balance - 10, cart: 'full'};
             case ACTIONS.CLEAR_CART:
                 return {...initialStore};
             case ACTIONS.CHECKOUT:
                 return {...state, cart: 'null'};
             default:
-                return store;
+                return state;
             }
         // if (action.type === 'AD_TO_CART') {
         //     return {...state, balance: state.balance - 10, cart: 'full'};
         //     }
         //     return state;
     }
-    export const store = createStore(
-        reduser,
-        composeEnhancers(
-            applyMiddleware()
+const store = createStore(
+        contactsReduser
+        // composeEnhancers(
+        //     applyMiddleware()
             // other store enhancers if any
-          )
-        );
+          );
+    
